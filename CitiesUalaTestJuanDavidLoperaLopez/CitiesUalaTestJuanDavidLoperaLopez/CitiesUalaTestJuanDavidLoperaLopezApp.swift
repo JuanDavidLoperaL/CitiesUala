eb72aec705f70df4a3d50664343df255e8b8b009
Juan David Lopera Lopez <juandavidl2011.jdll@gmail.com>
2025-02-05 16:45:43 -0500

    Initializing Project
    Creating project

---
+++ CitiesUalaTestJuanDavidLoperaLopez/CitiesUalaTestJuanDavidLoperaLopez/CitiesUalaTestJuanDavidLoperaLopezApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesUalaTestJuanDavidLoperaLopezApp.swift
+//  CitiesUalaTestJuanDavidLoperaLopez
+//
+//  Created by Juan david Lopera lopez on 5/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CitiesUalaTestJuanDavidLoperaLopezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitiesUalaTestJuanDavidLoperaLopez/CitiesUalaTestJuanDavidLoperaLopezTests/CitiesUalaTestJuanDavidLoperaLopezTests.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesUalaTestJuanDavidLoperaLopezTests.swift
+//  CitiesUalaTestJuanDavidLoperaLopezTests
+//
+//  Created by Juan david Lopera lopez on 5/02/25.
+//
+
+import Testing
+@testable import CitiesUalaTestJuanDavidLoperaLopez
+
+struct CitiesUalaTestJuanDavidLoperaLopezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
